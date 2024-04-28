74b2938999b9500852e2c67707b1a0a9aadbfd7f
rehanakabirmim <rehanakabirmim@gmail.com>
2024-04-28 21:47:55 +0600

    1st part done(navbar

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/about',function (){
+    return view('about');
+});
+
+Route::get('/services', function(){
+    return view('services');
+});
+Route::get('/contact', function(){
+    return view('contact');
+});
+
